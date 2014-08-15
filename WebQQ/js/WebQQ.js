4fb04275440d3756fcd106523112f13055732408
js8jxl07 <296528724@qq.com>
2014-08-15 11:36:45 +0800

    2014-08-15(1)
    1.webQQ.js

--- WebQQ/js/WebQQ.js
+++ WebQQ/js/WebQQ.js
@@ -1,20 +1,74 @@
 $(function(){
 	$("#talk").click(function(){
-		$(this).find(".icon-talk").css({"background-image":'url("./css/images/tab_icon_conversation_selected.png")'});
-
+		$(".icon-talk").css({"background-image":'url("./css/images/tab_icon_conversation_selected.png")'});
+		$(".icon-contact").css({"background-image":'url("./css/images/tab_icon_contact.png")'});
+		$(".icon-discover").css({"background-image":'url("./css/images/tab_icon_plugin.png")'});
+		$(".icon-setting").css({"background-image":'url("./css/images/tab_icon_setup.png")'});
+		$(".foot-nav li").removeClass("nav-selected");
+		$(this).addClass("nav-selected");
+		$(".panel").hide();
+		$("#panel-1").show();
+		
 	});
 	$("#contact").click(function(){
 		
-		$(this).find(".icon-contact").css({"background-image":'url("./css/images/tab_icon_contact_selected.png")'});
-		
+		$(".icon-talk").css({"background-image":'url("./css/images/tab_icon_conversation.png")'});
+		$(".icon-contact").css({"background-image":'url("./css/images/tab_icon_contact_selected.png")'});
+		$(".icon-discover").css({"background-image":'url("./css/images/tab_icon_plugin.png")'});
+		$(".icon-setting").css({"background-image":'url("./css/images/tab_icon_setup.png")'});
+		$(".foot-nav li").removeClass("nav-selected");
+		$(this).addClass("nav-selected");
+		$(".panel").hide();
+		$("#panel-2").show();
 	});
 	$("#discover").click(function(){
 		
-		$(this).find(".icon-discover").css({"background-image":'url("./css/images/tab_icon_plugin_selected.png")'});
-		
+		$(".icon-talk").css({"background-image":'url("./css/images/tab_icon_conversation.png")'});
+		$(".icon-contact").css({"background-image":'url("./css/images/tab_icon_contact.png")'});
+		$(".icon-discover").css({"background-image":'url("./css/images/tab_icon_plugin_selected.png")'});
+		$(".icon-setting").css({"background-image":'url("./css/images/tab_icon_setup.png")'});
+		$(".foot-nav li").removeClass("nav-selected");
+		$(this).addClass("nav-selected");
+		$(".panel").hide();
+		$("#panel-3").show();
 	});
 	$("#setting").click(function(){
 		
-		$(this).find(".icon-setting").css({"background-image":'url("./css/images/tab_icon_setup_selected.png")'});
+		$(".icon-talk").css({"background-image":'url("./css/images/tab_icon_conversation.png")'});
+		$(".icon-contact").css({"background-image":'url("./css/images/tab_icon_contact.png")'});
+		$(".icon-discover").css({"background-image":'url("./css/images/tab_icon_plugin.png")'});
+		$(".icon-setting").css({"background-image":'url("./css/images/tab_icon_setup_selected.png")'});
+		$(".foot-nav li").removeClass("nav-selected");
+		$(this).addClass("nav-selected");
+		$(".panel").hide();
+		$("#panel-4").show();
 	});
+	$(".friend").click(function(){
+		$(".memberTab li").removeClass("active");
+		$(".friend").addClass("active");
+		$("#haoyouList").show();
+		$("#qunList").hide();
+		$("#zuList").hide();
+	});
+	$(".group").click(function(){
+		$(".memberTab li").removeClass("active");
+		$(".group").addClass("active");
+		$("#haoyouList").hide();
+		$("#qunList").show();
+		$("#zuList").hide();
+	});
+	$(".discuss").click(function(){
+		$(".memberTab li").removeClass("active");
+		$(".discuss").addClass("active");
+		$("#haoyouList").hide();
+		$("#qunList").hide();
+		$("#zuList").show();
+	});
+
+	$(".groupTittle").click(function(){		
+		$(this).next().toggle();
+	});
+
+
+	
 });
