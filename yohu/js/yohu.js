2643adecc97620f1ffdb400da65e1ac704187d77
js8jxl07 <296528724@qq.com>
2014-08-22 17:09:13 +0800

    2014-08-22

--- yohu/js/yohu.js
+++ yohu/js/yohu.js
@@ -1,17 +1,22 @@
 $(function(){
-	$(".small-img li").hover(function(){
+	$(".nav-bar li").hover(function(){
 		$(this).addClass("cur");
+		$(this).find(".nav-panel").show();
 	},function(){
+		$(this).find(".nav-panel").hide();
 		$(this).removeClass("cur");
 	});
 
+	$("#btnBox .before").click(function(){
+		$("#recommend-1").toggle();
+		$("#recommend-2").toggle();
+	});
+
+	$("#btnBox .next").click(function(){
+		$("#recommend-1").toggle();
+		$("#recommend-2").toggle();
+	});
 	
-	// $("#foward").click(function(){
-
-	// });
-	// $("#goback").click(function(){
-
-	// });
 
 });
 
